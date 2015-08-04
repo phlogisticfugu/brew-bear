cfe8c666a1cdc041203293aee33cbfc14d3f2001
phlogisticfugu <phlogisticfugu@gmail.com>
2015-08-04 01:33:48 -0700

    checkpoint

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,25 @@
 var gulp = require('gulp');
+var del = require('del');
+var gitclone = require('./lib/gitclone.js');
+var path = require('path');
+var runSequence = require('run-sequence');
 
+var homebrewPath = path.join(__dirname, 'homebrew');
+
+gulp
+.task('clean', function(cb) {
+  del([
+    homebrewPath
+  ], cb);
+})
+.task('clone-homebrew', function() {
+  return gitclone(homebrewPath, 'https://github.com/Homebrew/homebrew');
+})
+.task('default', function(cb) {
+  runSequence(
+    'clean',
+    ['clone-homebrew'],
+    cb
+  );
+})
+;
